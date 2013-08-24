e80742a6c14f0a00ae4b65f7f91a8451422e223e
David Pan <pandavid87@gmail.com>
2013-08-23 23:09:59 -0700

    Initial Commit

---
+++ Pet Finder/DPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DPAppDelegate.h
+//  Pet Finder
+//
+//  Created by David Pan on 8/23/13.
+//  Copyright (c) 2013 David Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
