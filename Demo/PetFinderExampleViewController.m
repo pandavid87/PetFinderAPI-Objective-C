3a0c66247ef5468416fb941a7034a69742faffca
David Pan <pandavid87@gmail.com>
2013-09-05 15:51:53 -0700

    removed unnecessary code from examples

--- Demo/PetFinderExampleViewController.m
+++ Demo/PetFinderExampleViewController.m
@@ -16,16 +16,4 @@
 
 @implementation PetFinderExampleViewController
 
-
--(IBAction)test:(id)sender{
-    _petFinder = [[PetFinder alloc] initWithApiKey:@"ba963900373951971ad3e690062149d1" secret:@"eaefb47d85c4270ec321dc6c44dd6e07" defaultFormat:kPetFinderFormatJSON defaultOutput:kPetFinderOutputFull];
-    [_petFinder breedList:PetFinderAnimalTypeDog completionBlock:^(id response, NSError *error) {
-        if(!error){
-            NSLog(@"%@", [NSJSONSerialization JSONObjectWithData:response options:kNilOptions error:nil]);
-        }
-        else{
-            NSLog(@"%@", error);
-        }
-    }];
-}
 @end
